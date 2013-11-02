fabc466fc2da0c43483a5c64ab2993e83ec895db
George Machitidze <giomac@gmail.com>
2013-11-02 03:23:20 -0700

    Create gammuinject.sh

---
+++ gammuinject.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+usage(){
+    echo "Usage: $0 [MSISDN] [message] <body>"
+    exit 1
+}
+
+[[ $# -eq 0 ]] && usage
+
+to="$1"
+subj="$2"
+body="$3"
+
+sudo gammu-smsd-inject TEXT +$to -text "$subj"
